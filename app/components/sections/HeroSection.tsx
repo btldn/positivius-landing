c144734770e1e17c4aec4be1b879bff7e3d9c205
btldn <darkzell20@gmail.com>
2026-09-04 16:51:57 +0300

    feat: add trusted-by logos to hero section

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -1,16 +1,32 @@
 import Button from "@/app/components/ui/Button";
 import Image from 'next/image'
 import illustration from './images/Illustration.svg'
+import amazonLogo from './images/amazon-logo.svg'
+import drizzleLogo from './images/drizzle-logo.svg'
+import hubspotLogo from './images/hubspot-logo.svg'
+import notionLogo from './images/notion-logo.svg'
+import netflixLogo from './images/netflix-logo.svg'
+import zoomLogo from './images/zoom-logo.svg'
 
 export default function HeroSection () {
   return (
-    <div className="!mt-[70px] section-wrapper flex justify-between items-center">
-      <div className="flex flex-col gap-[35px] max-w-[530px]">
-        <h1 className="text-h1">Navigating the digital landscape for success</h1>
-        <p className="text-p max-w-[498px] !leading-[28px]">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-        <Button className='self-start'>Book a consultation</Button>
+    <div className="!mt-[70px] section-wrapper">
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-[35px] max-w-[530px]">
+          <h1 className="text-h1">Navigating the digital landscape for success</h1>
+          <p className="text-p max-w-[498px] !leading-[28px]">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <Button className='self-start'>Book a consultation</Button>
+        </div>
+        <Image src={illustration} width="600" height="515" alt="Illustration" />
       </div>
-      <Image src={illustration} width="600" height="515" alt="Illustration" />
+      <div className="flex flex-row justify-between mt-[70px]">
+        <Image className="grayscale" src={amazonLogo} width={125} height={48} alt="Amazon logo" />
+        <Image className="grayscale" src={drizzleLogo} width={127} height={48} alt="Drizzle logo" />
+        <Image className="grayscale" src={hubspotLogo} width={129} height={48} alt="Hubspot logo" />
+        <Image className="grayscale" src={notionLogo} width={146} height={48} alt="Notion logo" />
+        <Image className="grayscale" src={netflixLogo} width={126} height={48} alt="Netflix logo" />
+        <Image className="grayscale" src={zoomLogo} width={111} height={48} alt="Zoom logo" />
+      </div>
     </div>
   )
 
