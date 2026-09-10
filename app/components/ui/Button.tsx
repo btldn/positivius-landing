282c006ab5df93f81c2350bba97b636a53834d8e
btldn <darkzell20@gmail.com>
2026-09-10 12:15:25 +0300

    feat: support disabled state on Button

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -4,10 +4,11 @@ type ButtonProps = {
   children: ReactNode;
   variant?: 'primary' | 'secondary' | 'tertiary'
   className?: string;
+  disabled?: boolean;
 }
 
 const variantClasses = {
-  primary: 'bg-(--color-primary) text-white hover:opacity-70',
+  primary: 'bg-(--color-primary) text-white hover:opacity-85',
   secondary: 'bg-transparent border-[1px] hover:bg-(--color-primary) hover:text-white',
   tertiary: 'bg-(--color-accent)'
 }
@@ -15,12 +16,14 @@ const variantClasses = {
 export default function Button({
   children,
   variant = 'primary',
-  className
+  className,
+  disabled,
 }: ButtonProps) {
   return (
     <>
       <button
-        className={`px-[35px] py-[20px] text-button rounded-[14px] cursor-pointer duration-400  ${variantClasses[variant]} ${className}`}
+        disabled={disabled}
+        className={`px-[35px] py-[20px] text-button rounded-[14px] cursor-pointer duration-400 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:opacity-50 ${variantClasses[variant]} ${className}`}
       >
         {children}
       </button>
